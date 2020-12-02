8ebd9208430cab768c88ea2cc94453f51c6060d9
Thomas Dawson <dawsontc003@gmail.com>
2020-12-02 13:17:59 -0700

    updated Employee.js file to create constructor function object

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+// TODO: Write code to define and export the Employee class
+
+// Create employee object from constructor function
+function Employee(name, id, email) {
+  this.name = name;
+  this.id = id;
+  this.email = email;
+}
+// Validation of constructor function creating an object
+// const e = new Employee("Thomas", 01, "dawson@code.com");
+// console.log(e);
+
+// setting getName() function
+// function getName() {
+//   console.log(this.name);
+// }
+
+module.exports = Employee;
