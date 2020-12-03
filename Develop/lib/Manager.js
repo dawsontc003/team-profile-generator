85e0aaea44c101734436ebceade3fed4bece8c1d
Thomas Dawson <dawsontc003@gmail.com>
2020-12-02 23:07:26 -0700

    Updated Manager JS to pass all tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+function Manager(name, id, email, officeNumber) {
+  this.officeNumber = officeNumber;
+  this.getOfficeNumber = () => {
+    return officeNumber;
+  };
+  this.getRole = () => {
+    return "Manager";
+  };
+}
+
+module.exports = Manager;
