273e053e2b79603bb4252e8bf4c7b9e8f26aa0d5
Thomas Dawson <dawsontc003@gmail.com>
2020-12-02 22:54:51 -0700

    Updated Intern JS to pass 3 test suites and 14 scenarios

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+function Intern(name, id, email, school) {
+  this.school = school;
+  this.getSchool = () => {
+    return school;
+  };
+  this.getRole = () => {
+    return "Intern";
+  };
+}
+
+// const e = new Intern("Thomas", 01, "dawson@code.com", "DU");
+// console.log(e);
+
+// const e = new Intern("Thomas", 01, "dawson@code.com", "DU");
+// console.log(e);
+
+module.exports = Intern;
